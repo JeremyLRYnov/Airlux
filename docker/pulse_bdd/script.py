fa5bdf118494705bf0362c2be1f68ac727127a3b
alexisbrouard <alexis.brouard31@outlook.fr>
2022-11-04 10:47:49 +0100

    modifié :         docker/docker-compose.yml 	modifié :         docker/pulse_bdd/Dockerfile 	modifié :         docker/pulse_bdd/script.py

--- docker/pulse_bdd/script.py
+++ docker/pulse_bdd/script.py
@@ -6,39 +6,47 @@ redis = redis.Redis(
     host= 'redis',
     port= '6379')
 
-async def main():
-    temperature = random.randint(0, 40)
-    humidity = random.randint(0, 100)
-    while(True):
-        redis.mset({'temperature' : temperature, 'humidity' : humidity})
-        redis.get('temperature')
-        redis.get('humidity')
+temperature = random.randint(0, 40)
+humidity = random.randint(0, 100)
 
-        temperature = await set_humidity(5000,temperature)
-        humidity = await set_temperature(5000,humidity)
+while(True):
+    redis.mset({'temperature' : temperature, 'humidity' : humidity})
+    value = redis.get('temperature')
+    print(value)
+    value = redis.get('humidity')
+    print(value)
 
-async def set_humidity(delay, value):
-    await asyncio.sleep(delay)
-    random_value = random.randint(0,1)
-    if random_value == 0 :
-        value = value + 1
-    else:
-        value = value - 1
-    if value > 100:
-        value = 100
-    if value < 0:
-        value = 0
-    return value
+# async def set_temperature(delay, value):
+#     await asyncio.sleep(delay)
+#     random_value = random.randint(0,1)
+#     if random_value == 0 :
+#         value = value + 1
+#     else:
+#         value = value - 1
+#     if value > 50:
+#         value = 50
+#     if value < -10:
+#         value = -10
+#     return value
+
+#         value = value + 1
+#     else:
+#         value = value - 1   
+#     if value > 100:
+#         value = 100
+#     if value < 0:
+#         value = 0
+#     return value
         
-async def set_temperature(delay, value):
-    await asyncio.sleep(delay)
-    random_value = random.randint(0,1)
-    if random_value == 0 :
-        value = value + 1
-    else:
-        value = value - 1
-    if value > 50:
-        value = 50
-    if value < -10:
-        value = -10
-    return value
+# async def set_temperature(delay, value):
+#     await asyncio.sleep(delay)
+#     random_value = random.randint(0,1)
+#     if random_value == 0 :
+#         value = value + 1
+#     else:
+#         value = value - 1
+#     if value > 50:
+#         value = 50
+#     if value < -10:
+#         value = -10
+#     return value
