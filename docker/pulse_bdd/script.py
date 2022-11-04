5b957edd53bbadcaf5fd02beda3f24cdcfca6ac7
alexisbrouard <alexis.brouard31@outlook.fr>
2022-11-04 11:22:01 +0100

    modifié :         docker/pulse_bdd/script.py

--- docker/pulse_bdd/script.py
+++ docker/pulse_bdd/script.py
@@ -1,52 +1,49 @@
 import redis
 import random
 import asyncio
+import time
 
 redis = redis.Redis(
     host= 'redis',
     port= '6379')
 
-temperature = random.randint(0, 40)
-humidity = random.randint(0, 100)
+async def main():
+    temperature = random.randint(0, 40)
+    humidity = random.randint(0, 100)
+    while(True):
+        redis.mset({'temperature' : temperature, 'humidity' : humidity})
+        value = redis.get('temperature')
+        print(value)
+        value = redis.get('humidity')
+        print(value)
 
-while(True):
-    redis.mset({'temperature' : temperature, 'humidity' : humidity})
-    value = redis.get('temperature')
-    print(value)
-    value = redis.get('humidity')
-    print(value)
+        temperature = await set_temperature(temperature)
+        humidity = await set_humidity(humidity)
 
-# async def set_temperature(delay, value):
-#     await asyncio.sleep(delay)
-#     random_value = random.randint(0,1)
-#     if random_value == 0 :
-#         value = value + 1
-#     else:
-#         value = value - 1
-#     if value > 50:
-#         value = 50
-#     if value < -10:
-#         value = -10
-#     return value
+        time.sleep(10)
 
-#         value = value + 1
-#     else:
-#         value = value - 1   
-#     if value > 100:
-#         value = 100
-#     if value < 0:
-#         value = 0
-#     return value
+async def set_temperature(value):
+    random_value = random.randint(0,1)
+    if random_value == 0 :
+        value = value + 1
+    else:
+        value = value - 1
+    if value > 50:
+        value = 50
+    if value < -10:
+        value = -10
+    return value
         
-# async def set_temperature(delay, value):
-#     await asyncio.sleep(delay)
-#     random_value = random.randint(0,1)
-#     if random_value == 0 :
-#         value = value + 1
-#     else:
-#         value = value - 1
-#     if value > 50:
-#         value = 50
-#     if value < -10:
-#         value = -10
-#     return value
+async def set_humidity(value):
+    random_value = random.randint(0,1)
+    if random_value == 0 :
+        value = value + 1
+    else:
+        value = value - 1
+    if value > 50:
+        value = 50
+    if value < -10:
+        value = -10
+    return value
+
+asyncio.run(main())
