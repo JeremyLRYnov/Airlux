f3272dc33b425fdc2509ec060344fa1f00457fc7
alexisbrouard <alexis.brouard31@outlook.fr>
2022-11-10 11:01:03 +0100

    modifié :         docker/pulse_bdd/script.py 	modifié :         docker/reader/Dockerfile

--- docker/pulse_bdd/script.py
+++ docker/pulse_bdd/script.py
@@ -6,7 +6,7 @@ import time
 redis = redis.Redis(
     host= 'redis',
     port= '6379')
-
+    
 async def main():
     temperature = random.randint(0, 40)
     humidity = random.randint(0, 100)
@@ -18,12 +18,26 @@ async def main():
         print(value)
 
         temperature = await set_temperature(temperature)
-        humidity = await set_humidity(humidity)
+        humidity = await set_humidity(humidity) 
+
+        # Creation d'une instance PubSub
+        p = redis.pubsub()
+
+        # Inscription au canal "test"
+        p.subscribe('test')
+
+        # Publication d'un message sur le canal "test"
+        redis.publish('test', 'This is a test message')
+
+        print(p.get_message())
+
+        # Desinscription du canal "test"
+        p.unsubscribe('test')
 
         time.sleep(10)
 
 async def set_temperature(value):
-    random_value = random.randint(0,1)
+    random_value = random.randint(0,2)
     if random_value == 0 :
         value = value + 1
     if random_value == 1:
@@ -51,7 +65,7 @@ async def set_humidity(value):
         value = 50
     if value < -10:
         value = -10
-        
+
     return value
 
 asyncio.run(main())
