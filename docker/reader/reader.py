da3794b452f373290ea92774daae2e123df431d8
alexisbrouard <alexis.brouard31@outlook.fr>
2022-11-10 11:22:18 +0100

    modifié :         docker/reader/reader.py

--- docker/reader/reader.py
+++ docker/reader/reader.py
@@ -1,1 +1,17 @@
+import redis
+import asyncio
 
+redis = redis.Redis(
+    host='redis', 
+    port=6379)
+
+async def main():
+    p = redis.pubsub()
+    p.subscribe('temperature')
+    p.subscribe('humidity')
+    while True:
+        message = p.get_message()
+        if message:
+            print(message)
+
+asyncio.run(main())
