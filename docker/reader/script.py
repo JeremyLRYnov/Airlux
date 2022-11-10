ec4941b7259996c7bbdc4284ac8f99e0f99af61a
alexisbrouard <alexis.brouard31@outlook.fr>
2022-11-10 15:10:32 +0100

    modifié :         docker/cloud_server/Dockerfile 	modifié :         docker/docker-compose.yml 	supprimé :        docker/local_server/dump.rdb 	nouveau fichier : docker/pulse_bdd/__pycache__/filter_script.cpython-39.pyc 	modifié :         docker/reader/Dockerfile 	modifié :         docker/reader/script.py

--- docker/reader/script.py
+++ docker/reader/script.py
@@ -1,13 +1,10 @@
 import mysql.connector
 
-def main():
 
-    mydb = mysql.connector.connect(
-        host="mysql",
-        user="root",)
+mydb = mysql.connector.connect(
+    host="mysql",
+    database="db")
     
-    mycursor = mydb.cursor()
-    mycursor.execute("CREATE DATABASE IF NOT EXISTS db")
-    mycursor.execute("USE db")
-    mycursor.execute("CREATE TABLE IF NOT EXISTS temperature (id INT AUTO_INCREMENT PRIMARY KEY, value INT)")
-    mycursor.execute("CREATE TABLE IF NOT EXISTS humidity (id INT AUTO_INCREMENT PRIMARY KEY, value INT)")
+mycursor = mydb.cursor()
+mycursor.execute("CREATE TABLE IF NOT EXISTS temperature (id INT AUTO_INCREMENT PRIMARY KEY, value INT)")
+mycursor.execute("CREATE TABLE IF NOT EXISTS humidity (id INT AUTO_INCREMENT PRIMARY KEY, value INT)")
