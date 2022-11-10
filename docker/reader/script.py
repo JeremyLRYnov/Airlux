b9cd45bba766ab9790cd9af9f31ecdd9cd2ebc30
alexisbrouard <alexis.brouard31@outlook.fr>
2022-11-10 16:36:15 +0100

    modifié :         reader/script.py

--- docker/reader/script.py
+++ docker/reader/script.py
@@ -3,7 +3,9 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
     host="mysql",
-    database="db")
+    user="user",
+    database="db",
+    port=3306)
     
 mycursor = mydb.cursor()
 mycursor.execute("CREATE TABLE IF NOT EXISTS temperature (id INT AUTO_INCREMENT PRIMARY KEY, value INT)")
