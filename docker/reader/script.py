0a4f639dcbcfc943ed97f380f3a4d039ec3751ab
jeremyLRYnov <jeremy.lacazerusques@ynov.com>
2022-11-17 12:28:05 +0100

    FIX user in docker compose

--- docker/reader/script.py
+++ docker/reader/script.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 # mysql Authentification
 mydb = mysql.connector.connect(
-    host="127.0.0.1",
+    host="mysql",
     port="3306",
     user="root",
     database="db")
@@ -11,3 +11,4 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 mycursor.execute("CREATE TABLE IF NOT EXISTS temperature (id INT AUTO_INCREMENT PRIMARY KEY, value INT)")
 mycursor.execute("CREATE TABLE IF NOT EXISTS humidity (id INT AUTO_INCREMENT PRIMARY KEY, value INT)")
+mydb.close()
