3c60e8aea1e2012e6b3c670c24ab80788ce83fc3
jeremyLRYnov <jeremy.lacazerusques@ynov.com>
2022-11-17 11:59:32 +0100

    UPDATE Docker compose and reader script

--- docker/reader/script.py
+++ docker/reader/script.py
@@ -2,9 +2,9 @@ import mysql.connector
 
 # mysql Authentification
 mydb = mysql.connector.connect(
-    host="mysql",
-    user="user",
-    password="",
+    host="127.0.0.1",
+    port="3306",
+    user="root",
     database="db")
     
 # mysql Channel
