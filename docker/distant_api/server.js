96a5f1d33b5b5d732a8441571674342346c8a0f1
alexisbrouard <alexis.brouard31@outlook.fr>
2022-11-25 10:47:44 +0100

    add working api parameters

--- docker/distant_api/server.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-
-// set port, listen for requests
-const PORT = process.env.NODE_DOCKER_PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
