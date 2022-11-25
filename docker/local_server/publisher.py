35ae071df1fb3f9b149001d2b3ad79de5b00ebd1
Zetsuy <jeremygarciamobile@gmail.com>
2022-11-25 15:44:39 +0100

    Sub marche si on ecrit pythone3 publisher.py dans terminal docker

--- docker/local_server/publisher.py
+++ docker/local_server/publisher.py
@@ -1,40 +1,21 @@
 import redis
 import paho.mqtt.client as mqtt
 
+#hostname
 broker="localhost"
-
+#port
 port=1883
-
+#time to live
 timelive=60
-
-#Initialisation de l'instance
-r = redis.Redis(
-    host=redis,
-    port=6379,
-    decode_responses=True # <-- this will ensure that binary data is decoded
-)
 
 def on_connect(client, userdata, flags, rc):
   print("Connected with result code "+str(rc))
   client.subscribe("/temperature")
-  
 def on_message(client, userdata, msg):
     print(msg.payload.decode())
-    redis.mset({'temperature' : msg.payload.decode()})
-
-            #check that the data is present on the Redis database
-    value = redis.get('temperature')
-    print(value)
     
 client = mqtt.Client()
 client.connect(broker,port,timelive)
 client.on_connect = on_connect
 client.on_message = on_message
 client.loop_forever()
-
-
-
-# Publication d'un message sur le canal "temperature"
-message = "This is a test message"
-r.publish('temperature', message)
-
