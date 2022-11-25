118fcdd84752cf90788fa4cbdac3e90b3cf86182
alexisbrouard <alexis.brouard31@outlook.fr>
2022-11-25 16:26:34 +0100

    access db [works]

--- docker/distant_api/app/models/db.js
+++ docker/distant_api/app/models/db.js
@@ -1,11 +1,18 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const dbConfig = require("../config/db.config.js");
 
+// Create a connection to the database
 var connection = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   database: dbConfig.DB
+});
+
+// open the MySQL connection
+connection.getConnection(error => {
+  if (error) throw error;
+  console.log("Successfully connected to the database.");
 });
 
 module.exports = connection;
--- docker/distant_api/server.js
+++ docker/distant_api/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-// const bodyParser = require("body-parser"); /* deprecated */
 const cors = require("cors");
 
 const app = express();
@@ -9,21 +8,16 @@ var corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(express.json());
 
-// parse requests of content-type - application/json
-app.use(express.json()); /* bodyParser.json() is deprecated */
+app.use(express.urlencoded({ extended: true }));
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true })); /* bodyParser.urlencoded() is deprecated */
-
-// simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
 require("./app/routes/tutorial.routes.js")(app);
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
