e4cc8902da3d39839caa957f3f6df6b4b1974397
Zetsuy <jeremygarciamobile@gmail.com>
2022-12-02 09:17:50 +0100

    Pub/Sub + Insertion Redis temperature

--- docker/local_insertion/publisher.py
+++ docker/local_insertion/publisher.py
@@ -1,6 +1,8 @@
 import redis
 import paho.mqtt.client as mqtt
 
+r = redis.StrictRedis('redis', 6379, charset="utf-8", decode_responses=True)
+
 #hostname
 broker="mosquitto"
 #port
@@ -8,11 +10,18 @@ port=1883
 #time to live
 timelive=60
 
+
+
 def on_connect(client, userdata, flags, rc):
   print("Connected with result code "+str(rc))
   client.subscribe("/temperature")
 def on_message(client, userdata, msg):
     print(msg.payload.decode())
+    r.mset({'temperature' : msg.payload.decode()})
+
+    #check that the data is present on the Redis database
+    value = r.get('temperature')
+    print(value)
     
 client = mqtt.Client()
 
