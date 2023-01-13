2fb8a4fb74d506eb0db6bd5b14ebd4786dd2aeda
Zetsuy <jeremygarciamobile@gmail.com>
2023-01-13 14:33:51 +0100

    Connection au WiFI + ping de l'ordinateur

--- esp32_functions/WiFiClient/WiFiClient.ino
+++ esp32_functions/WiFiClient/WiFiClient.ino
@@ -1,94 +1,49 @@
-/*
- *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
- *
- *  You need to get streamId and privateKey at data.sparkfun.com and paste them
- *  below. Or just customize this script to talk to other HTTP servers.
- *
- */
-
 #include <WiFi.h>
+#include <ESP32Ping.h>
 
-const char* ssid     = "NomReseau";
-const char* password = "MotDePasseReseau";
+const char* ssid = "JeremyPointAcces"; //a changer selon wifi
+const char* password = "ewhj5675"; //a changer selon wifi
 
-const char* host = "google.com";
-const char* streamId   = "....................";
-const char* privateKey = "....................";
+IPAddress ip (192, 168, 0, 40); // Ip du pc a changer a chaque fois
 
-void setup()
-{
+
+String get_wifi_status(int status){
+    switch(status){
+        case WL_IDLE_STATUS:
+        return "WL_IDLE_STATUS";
+        case WL_SCAN_COMPLETED:
+        return "WL_SCAN_COMPLETED";
+        case WL_NO_SSID_AVAIL:
+        return "WL_NO_SSID_AVAIL";
+        case WL_CONNECT_FAILED:
+        return "WL_CONNECT_FAILED";
+        case WL_CONNECTION_LOST:
+        return "WL_CONNECTION_LOST";
+        case WL_CONNECTED:
+        return "WL_CONNECTED";
+        case WL_DISCONNECTED:
+        return "WL_DISCONNECTED";
+    }
+}
+
+void setup(){
     Serial.begin(115200);
-    delay(10);
-
-    // We start by connecting to a WiFi network
-
-    Serial.println();
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-
+    delay(1000);
+    int status = WL_IDLE_STATUS;
+    Serial.println("\nConnecting");
+    Serial.println(get_wifi_status(status));
     WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED) {
+    while(status != WL_CONNECTED){
         delay(500);
-        Serial.print(".");
+        status = WiFi.status();
+        Serial.println(get_wifi_status(status));
     }
 
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
+    Serial.println("\nConnected to the WiFi network");
+    Serial.print("Local ESP32 IP: ");
     Serial.println(WiFi.localIP());
 }
 
-int value = 0;
+void loop(){
 
-void loop()
-{
-    delay(5000);
-    ++value;
-
-    Serial.print("connecting to ");
-    Serial.println(host);
-
-    // Use WiFiClient class to create TCP connections
-    WiFiClient client;
-    const int httpPort = 80;
-    if (!client.connect(host, httpPort)) {
-        Serial.println("connection failed");
-        return;
-    }
-
-    // We now create a URI for the request
-    String url = "/input/";
-    url += streamId;
-    url += "?private_key=";
-    url += privateKey;
-    url += "&value=";
-    url += value;
-
-    Serial.print("Requesting URL: ");
-    Serial.println(url);
-
-    // This will send the request to the server
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + host + "\r\n" +
-                 "Connection: close\r\n\r\n");
-    unsigned long timeout = millis();
-    while (client.available() == 0) {
-        if (millis() - timeout > 5000) {
-            Serial.println(">>> Client Timeout !");
-            client.stop();
-            return;
-        }
-    }
-
-    // Read all the lines of the reply from server and print them to Serial
-    while(client.available()) {
-        String line = client.readStringUntil('\r');
-        Serial.print(line);
-    }
-
-    Serial.println();
-    Serial.println("closing connection");
 }
-
