151410d56288d4d35314788f2dca09f2a1a45241
DaoGod <58519024+DaoGod@users.noreply.github.com>
2023-01-27 12:05:27 +0100

    petit update API distant

--- docker/distant_api/app/models/db.js
+++ docker/distant_api/app/models/db.js
@@ -12,17 +12,6 @@ var connection = mysql.createPool({
 // open the MySQL connection
 connection.getConnection(error => {
   if (error) throw error;
-
-  connection.query("CREATE TABLE IF NOT EXISTS Temperature (id INT, value INT);", function (err, result){
-    if (err) throw err;
-    console.log("Create table Temperature.");
-  });
-  
-  connection.query("CREATE TABLE IF NOT EXISTS Humidity (id INT, value INT);", function (err, result){
-    if (err) throw err;
-    console.log("Create table Humidity.");
-  });
-
   console.log("Successfully connected to the database.");
 });
 
