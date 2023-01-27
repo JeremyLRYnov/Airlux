1b2a2a445e33ec1cdfda9e7ba0eb9f27cfcaa48d
alexisbrouard <alexis.brouard31@outlook.fr>
2023-01-27 15:45:12 +0100

    add Home Page - over [works]

--- mobileapp/lib/models/constants.dart
+++ mobileapp/lib/models/constants.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryBlue = Color(0xFF295299);
+const kPrimaryButtonBlue = Color(0xFF04567F);
+const kPrimaryButtonInactive = Color(0xFF073954);
+const kPrimaryButtonActive = Color(0xFF254B8C);
