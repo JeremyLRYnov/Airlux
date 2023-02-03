c8567aa07e2e082880a82400a78197aaf1a0208c
Alexis Brouard <67193389+alexisbrouard@users.noreply.github.com>
2023-02-03 16:53:18 +0100

    refacto widgets

---
+++ mobileapp/lib/widgets/rouded_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'package:mobileapp/models/constants.dart';
+
+
+class RoudedButton extends StatelessWidget {
+  const RoudedButton({
+    Key? key,
+    required this.title,
+    required this.onPressed,
+  }) : super(key: key);
+  final String title;
+  final void Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: kPrimaryBlue,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: onPressed,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            title,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
