39e4f491ed9c626bffc1abf987804f60310e73d1
Nicolas Seillé <70601970+Nicolas-3050@users.noreply.github.com>
2023-03-10 16:41:55 +0100

    tests signin, delete

--- docker/local_api/app/test/utils2.test.js
+++ docker/local_api/app/test/utils2.test.js
@@ -1,5 +1,6 @@
 const axios = require('axios').default;
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 //Controller 
 describe('Controller User', () => {
@@ -38,5 +39,49 @@ describe('Controller User', () => {
     expect(user.admin).toEqual(false);
     const passwordsMatch = await bcrypt.compare("autttt..", user.password);
     expect(passwordsMatch).toBe(true);
-});
+  });
+
+  it('Signin POST /', async () => {
+    const response = await axios.post('http://localhost:6869/user/signin', {
+      email: 'nicolas@example.com',
+      password: 'autttt..'
+    });
+    expect(response.status).toEqual(200);
+    expect(response.data.message).toEqual('Connexion réussi');
+    expect(response.data.result).toBeDefined();
+    expect(response.data.token).toBeDefined();
+  
+    // Vérification des données renvoyées
+    const responseData = response.data.result;
+    expect(responseData.name).toEqual('nicolas');
+    expect(responseData.email).toEqual('nicolas@example.com');
+    expect(responseData.admin).toEqual(false);
+  
+  });
+
+  it('Delete/', async () => {
+    // création de l'utilisateur
+    const signupResponse = await axios.post('http://localhost:6869/user/signup', {
+      name: 'john',
+      email: 'john@example.com',
+      password: 'password',
+      admin: false
+    });
+  
+    // récupération de l'ID de l'utilisateur créé
+    const userId = signupResponse.data.result.id;
+  
+    // suppression de l'utilisateur
+    const response = await axios.delete(`http://localhost:6869/user/${userId}`);
+    expect (response.data).toEqual({
+        "result": "User "+userId+" deleted successfully."
+    })
+    expect(response.status).toEqual(200);
+  
+    // vérification que l'utilisateur a bien été supprimé
+    const getResponse = await axios.get('http://localhost:6869/user');
+    const users = getResponse.data.result;
+    const user = users.find(u => u.id === userId);
+    expect(user).toBeUndefined();
+  });
 });
