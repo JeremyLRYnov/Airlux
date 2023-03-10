c479f682ea83ed4ccb7f7fac250ab136f15d7d8f
alexisbrouard <alexis.brouard31@outlook.fr>
2023-03-10 10:04:00 +0100

    modified:   mobileapp/lib/screens/Welcome_screen.dart 	modified:   mobileapp/lib/widgets/footer_menu.dart

--- mobileapp/lib/screens/Welcome_screen.dart
+++ mobileapp/lib/screens/Welcome_screen.dart
@@ -13,45 +13,57 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Container(
-                  child: Image.asset('assets/images/logo.png'),
-                  height: 145.0,
+    return WillPopScope(
+      onWillPop: () async {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content:
+            Text('Vous ne pouvez pas faire de Retour Arrière sur cette page.'),
+            backgroundColor: Colors.red,
+          ),
+        );
+        return false;
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Container(
+                    child: Image.asset('assets/images/logo.png'),
+                    height: 145.0,
 
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            RoudedButton(
-              title: 'Log In',
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Login()),
-                );
-              },
-            ),
-            RoudedButton(
-              title: 'Register',
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Register()),
-                );
-              },
-            ),
-          ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              RoudedButton(
+                title: 'Log In',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Login()),
+                  );
+                },
+              ),
+              RoudedButton(
+                title: 'Register',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Register()),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
