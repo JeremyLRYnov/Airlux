deea5aa156bdb399f3c2e45fe963dade5dd9a408
Zetsuy <jeremygarciamobile@gmail.com>
2023-03-17 12:41:56 +0100

    Login function work

--- mobileapp/lib/services/login_service.dart
+++ mobileapp/lib/services/login_service.dart
@@ -2,10 +2,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class LoginService {
-  static Future<String> login(String username, String password) async {
-    var url = Uri.parse('http://localhost:6869/user/signin');
+  static Future<String> login(String email, String password) async {
+    var url = Uri.parse('http://10.0.2.2:6869/user/signin');
     var response = await http
-        .post(url, body: {'username': username, 'password': password});
+        .post(url, body: {'email': email, 'password': password});
     if (response.statusCode == 200) {
       return jsonDecode(response.body)['token'];
     } else {
