80c6a2f714a110429705536753a6142a5ef3e783
jeremyLRYnov <jeremy.lacazerusques@ynov.com>
2023-03-29 11:45:05 +0200

    ADD building, room, sensor & switch models

---
+++ docker/local_api/app/models/building.models.js
@@ -0,0 +1,21 @@
+import { Entity, Schema } from 'redis-om'
+import client from '../config/client.js'
+
+/* our entity */
+class Building extends Entity {};
+
+/* create a Schema for Building */
+const buildingSchema = new Schema(Building, {
+  name: { type: 'string' },
+  createdBy: { type: 'string' },
+  users: { type: 'string[]' }
+},
+{
+  dataStructure: 'JSON'
+})
+
+/* use the client to create a Repository just for Buildings */
+export const buildingRepository = client.fetchRepository(buildingSchema)
+
+/* create the index for Building for search */
+await buildingRepository.createIndex('buildingIndex')
---
+++ docker/local_api/app/models/switch.models.js
@@ -0,0 +1,21 @@
+import { Entity, Schema } from 'redis-om'
+import client from '../config/client.js'
+
+/* our entity */
+class Switch extends Entity {};
+
+/* create a Schema for Switch */
+const switchSchema = new Schema(Switch, {
+  name: { type: 'string' },
+  roomId: { type: 'string' },
+  status: { type: 'boolean' }
+},
+{
+  dataStructure: 'JSON'
+})
+
+/* use the client to create a Repository just for Switchs */
+export const switchRepository = client.fetchRepository(switchSchema)
+
+/* create the index for Switch for search */
+await switchRepository.createIndex('switchIndex')
