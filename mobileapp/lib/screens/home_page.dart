719aa148a1a69c0afee3fd79d2d49c28ef4004b3
Alexis Brouard <67193389+alexisbrouard@users.noreply.github.com>
2023-09-17 18:59:31 +0200

    add rooms

--- mobileapp/lib/screens/home_page.dart
+++ mobileapp/lib/screens/home_page.dart
@@ -23,26 +23,30 @@ class _HomePageState extends State<HomePage> {
       selectedIndex = index;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     final roomProvider = Provider.of<RoomProvider>(context);
     final roomList = roomProvider.rooms;
     return WillPopScope(
-        onWillPop: () async {
-          return false;
-        },
-    child: Scaffold(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Expanded(
               flex: 4,
               child: Container(
-                margin: const EdgeInsets.only(top: 30.0, left: 10.0, right: 10.0),
+                margin:
+                    const EdgeInsets.only(top: 30.0, left: 10.0, right: 10.0),
                 child: SizedBox(
                   child: Center(
-                    child: Image.asset('assets/images/logo.png',
-                        width: double.infinity,),
+                    child: Image.asset(
+                      'assets/images/logo.png',
+                      width: double.infinity,
+                    ),
                   ),
                 ),
               ),
@@ -63,98 +67,102 @@ class _HomePageState extends State<HomePage> {
                 margin: const EdgeInsets.only(left: 40.0),
                 height: 400,
                 width: double.infinity,
-                child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 10.0),
-                    child: const Text(
-                      'Appareils',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 10.0),
+                      child: const Text(
+                        'Appareils',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: [
-                        Column(
-                          children: const [
-                            DeviceItem(
-                              icon: Icons.ac_unit_rounded,
-                              text: "température",
-                              color: kPrimaryButtonActive,
-                            ),
-                            DeviceItem(
-                              icon: Icons.light_rounded,
-                              text: "lumière",
-                              color: kPrimaryButtonInactive,
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: const [
-                            DeviceItem(
-                              icon: Icons.water_rounded,
-                              text: "humidité",
-                              color: kPrimaryButtonInactive,
-                            ),
-                            DeviceItem(
-                              icon: Icons.bolt_rounded,
-                              text: "prise",
-                              color: kPrimaryButtonActive,
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: const [
-                            DeviceItem(
-                              icon: Icons.tv_rounded,
-                              text: "télevision",
-                              color: kPrimaryButtonInactive,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 10.0),
-                    child: const Text(
-                      'Pièces',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+                    Expanded(
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          Column(
+                            children: const [
+                              DeviceItem(
+                                icon: Icons.ac_unit_rounded,
+                                text: "température",
+                                color: kPrimaryButtonActive,
+                              ),
+                              DeviceItem(
+                                icon: Icons.light_rounded,
+                                text: "lumière",
+                                color: kPrimaryButtonInactive,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: const [
+                              DeviceItem(
+                                icon: Icons.water_rounded,
+                                text: "humidité",
+                                color: kPrimaryButtonInactive,
+                              ),
+                              DeviceItem(
+                                icon: Icons.bolt_rounded,
+                                text: "prise",
+                                color: kPrimaryButtonActive,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: const [
+                              DeviceItem(
+                                icon: Icons.tv_rounded,
+                                text: "télevision",
+                                color: kPrimaryButtonInactive,
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: roomList.length,
-                      itemBuilder: (context, index) {
-                        final room = roomList[index];
-                        return RoomItem(
-                          text: room.name,
-                          piece: room.imageUrl,
-                          isselected: selectedIndex == index,
-                          width: 150,
-                          height: 150,
-                          onpressed: () {
-                            _onItemSelected(index);
-                          },
-                        );
-                      },
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 10.0),
+                      child: const Text(
+                        'Pièces',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                  ),
-                ],),
+                    Expanded(
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: roomList.length,
+                        itemBuilder: (context, index) {
+                          final room = roomList[index];
+                          return RoomItem(
+                            text: room.name,
+                            piece: room.imageUrl,
+                            isselected: selectedIndex == index,
+                            width: 150,
+                            height: 150,
+                            onpressed: () {
+                              _onItemSelected(index);
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    )
+                  ],
+                ),
               ),
             ),
           ],
         ),
-    ),
+      ),
     );
   }
 }
-
--- mobileapp/lib/widgets/room_item.dart
+++ mobileapp/lib/widgets/room_item.dart
@@ -9,6 +9,7 @@ class RoomItem extends StatelessWidget {
   final String piece;
   final bool isselected;
   final VoidCallback onpressed;
+
   const RoomItem({
     super.key,
     required this.width,
@@ -25,7 +26,9 @@ class RoomItem extends StatelessWidget {
       onTap: onpressed,
       child: Container(
         margin: const EdgeInsets.only(
-            right: 10.0, top: 10.0, bottom: 40.0,),
+          right: 10.0,
+          top: 10.0,
+        ),
         width: width,
         height: height,
         decoration: BoxDecoration(
@@ -35,19 +38,25 @@ class RoomItem extends StatelessWidget {
         child: Column(
           children: [
             ClipRRect(
-              borderRadius: const BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-              child: Image.asset("assets/images/$piece",
-              width: double.infinity,),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
+              child: Image.asset(
+                "assets/images/$piece",
+                width: double.infinity,
+              ),
             ),
             Container(
               margin: const EdgeInsets.all(15),
             ),
-            Text(text,
-            style: const TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
+            Text(
+              text,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
             ),
           ],
         ),
@@ -55,4 +64,3 @@ class RoomItem extends StatelessWidget {
     );
   }
 }
-
